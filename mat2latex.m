760d1b92387451b45916c57f2a17daaf98ccca21
Kyre <shinkyre@163.com>
2022-11-04 14:03:06 +0800

    Optimize the judgement of \sqrt

--- mat2latex.m
+++ mat2latex.m
@@ -1,4 +1,8 @@
 % This converts a matrix to latex printout.
+% Usage:
+% mat2latex(x) for a decimal output
+% mat2latex(sym(x)) for an output with symbols
+
 function s = mat2latex(x)
 format rat;
 [vert, hori] = size(x);
@@ -12,8 +16,13 @@ end
 s = s+"\end{matrix}\right)";
 
 % fromat \sqrt
-s = regexprep(s, "([\w\(\)+])\^\(1/2\)", "\\sqrt{$1}");
+% matches 'word' and '(w + o / r - d)'
+s = regexprep(s, "(?<num>([\w]+)|(\(\w[\w\(\)+-*/ ]+\)))\^\(1/2\)", "\\sqrt{$1}");
 % format exponential
 s = regexprep(s, "\^\(([0-9a-zA-Z/]+)\)", "\^{$1}"); 
 % format fraction
-s = regexprep(s, "(\d)/(\d)", "\\frac{$1}{$2}");
+s = regexprep(s, "(?<num>([\w]+)|(\(\w[\w\(\)+-*/ ]+\)))/(?<num>([\w]+)|(\(\w[\w\(\)+-*/ ]+\)))", "\\frac{$1}{$2}");
+
+
+
+
